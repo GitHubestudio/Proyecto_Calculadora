4fd927ffd97b874acac8ff5d3fb6977c32dcca9a
GitHubestudio <176661583+GitHubestudio@users.noreply.github.com>
2025-03-03 14:10:50 -0300

    proyecto calculadora basica terminado

---
+++ calculadora/src/componentes/Pantalla.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../hojas-de-estilo/Pantalla.css"
+
+function Pantalla({ input }){
+
+  return(
+    <div className="input">
+      {input}
+    </div>
+  );
+}
+
+export default Pantalla;
